d2d3f41ed022c01a798aba292e54e0306779f336
t.gourlaouen <t.gourlaouen@epackpro.com>
2025-03-14 14:04:24 +0100

    Main page todo

--- lib/domain/todo_entity.dart
+++ lib/domain/todo_entity.dart
@@ -1,35 +1,43 @@
 sealed class TodoEntity {
-  const TodoEntity({required this.title});
+  const TodoEntity({required this.title, required this.subtitle});
   final String title;
+  final String subtitle;
 }
 
 class LowTodoEntity extends TodoEntity {
-  const LowTodoEntity({required super.title});
+  const LowTodoEntity({
+    required super.title,
+    required super.subtitle,
+  });
 }
 
 class MediumTodoEntity extends TodoEntity {
   const MediumTodoEntity({
     required super.title,
-    required this.subtitle,
+    required super.subtitle,
   });
-  final String subtitle;
 }
 
 class HighTodoEntity extends TodoEntity {
-  const HighTodoEntity({required super.title});
+  const HighTodoEntity({
+    required super.title,
+    required super.subtitle,
+  });
 }
 
 extension TodoEntityExt on TodoEntity {
   T when<T>({
-    required T Function(String) onLow,
+    required T Function(String, String) onLow,
     required T Function(String, String) onMedium,
-    required T Function(String) onHigh,
+    required T Function(String, String) onHigh,
   }) {
     return switch (this) {
-      LowTodoEntity(:final title) => onLow(title),
+      LowTodoEntity(:final title, :final subtitle) =>
+          onLow(title, subtitle),
       MediumTodoEntity(:final title, :final subtitle) =>
         onMedium(title, subtitle),
-      HighTodoEntity(:final title) => onHigh(title),
+      HighTodoEntity(:final title, :final subtitle) =>
+          onHigh(title, subtitle),
     };
   }
 }
